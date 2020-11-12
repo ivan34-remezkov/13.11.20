8214495e7223b2f2e3c479c93431e918ea9e4f02
ivan34-remezkov <74048301+ivan34-remezkov@users.noreply.github.com>
2020-11-13 00:15:29 +0400

    угадай число 13.11.20

---
+++ app/src/main/java/com/example/ygadaichislo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ygadaichislo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinisheed;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tvInfo = (TextView)findViewById(R.id.textView2);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button);
+
+        guess = (int)(Math.random()*100);
+        gameFinisheed = false;
+
+
+    }
+
+    public void onClick (View v){
+
+        if (!gameFinisheed){
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText(getResources().getString(R.string.behind));
+            if (inp == guess)
+            {
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinisheed=true;
+            }
+        }
+        else
+        {
+            guess = (int)Math.random()*100;
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinisheed=false;
+
+        }
+    }
+}
